6e9e8bf77b16d8fad410a60cdc5377409080bce9
Inga <inga@local.machine>
2023-02-08 13:44:03 +0200

    toggle theme done

---
+++ dark-theme/src/App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import MoonIcon from './components/icons/MoonIcon';
+import SunIcon from './components/icons/SunIcon';
+import Switch from './components/Switch';
+
+const App = () => {
+  const [theme, setTheme] = useState('dark');
+  const isDarkTheme = theme === 'dark';
+
+  const toggleTheme = () => {
+    setTheme(isDarkTheme ? 'light' : 'dark');
+  };
+  return (
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <StyledApp>
+        <SunIcon />
+        <Switch toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
+        <MoonIcon />
+        <Name>Neko</Name>
+        <Info>Username: Nyanko</Info>
+        <Info>Email: neko@gmail.com</Info>
+      </StyledApp>
+    </ThemeProvider>
+  );
+};
+
+export default App;
+
+const StyledApp = styled.div`
+  min-height: 100vh;
+  text-align: center;
+  padding-top: 10rem;
+  background-color: ${(props) => props.theme.body};
+  transition: all 0.25s ease;
+`;
+
+const Name = styled.h1`
+  margin: 1rem;
+  color: ${(props) => props.theme.title};
+`;
+const Info = styled.p`
+  margin: 1rem;
+  color: ${(props) => props.theme.subtitle};
+`;
+
+const darkTheme = {
+  body: '#1c1c1c',
+  title: '#fff',
+  subtitle: '#b6b6b6',
+  icon: '#b6b6b6',
+};
+const lightTheme = {
+  body: '#fff',
+  title: '#1c1c1c',
+  icon: '#1c1c1c',
+  subtitle: '#333',
+};
+
