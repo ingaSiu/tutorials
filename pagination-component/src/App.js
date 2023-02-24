7fe3cd99b2a192fc59345c8b507792568dcbc6ad
ingaSiu <ingasiud@gmail.com>
2023-02-24 20:32:59 +0200

    Simple pagination

---
+++ pagination-component/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Pagination from './components/Pagination';
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  return (
+    <div className="container">
+      <Pagination currentPage={currentPage} total={500} limit={20} onPageChange={(page) => setCurrentPage(page)} />
+    </div>
+  );
+};
+
+export default App;
+
