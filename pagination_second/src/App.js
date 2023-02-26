4951cac02593869650058e26dafb3dd577964d35
ingaSiu <ingasiud@gmail.com>
2023-02-26 11:33:50 +0200

    new tutorial

---
+++ pagination_second/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Example1 from './pagination_examples/Example1';
+import Example2 from './pagination_examples/Example2';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Example2 />
+    </div>
+  );
+};
+
+export default App;
+
